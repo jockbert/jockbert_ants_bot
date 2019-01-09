94e35bf7c395aa87627310b9a9fd22302bbbaa68
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2019-01-09 22:59:42 +0100

    Use simplified API mod use

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 extern crate ants_ai_challenge_api;
 extern crate rand;
 
-// TODO Simplify "imports"
-use ants_ai_challenge_api::game_parameters::GameParameters;
-use ants_ai_challenge_api::position::*;
-use ants_ai_challenge_api::world_state::WorldState;
-use ants_ai_challenge_api::Direction::*;
 use ants_ai_challenge_api::*;
 use rand::Rng;
 
