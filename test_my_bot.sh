5e7b793d57619b04d343d5e1e73403c20b490a4b
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2019-02-07 07:13:30 +0100

    Format test script

--- test_my_bot.sh
+++ test_my_bot.sh
@@ -3,5 +3,6 @@
 RUST_BACKTRACE=1 cargo build && \
 cp target/debug/jockbot_ants_bot tools && \
 cd tools && \
-python ./playgame.py -l game_logs -m maps/maze/maze_p02_01.map -e --strict "./jockbot_ants_bot $@" "python sample_bots/python/HunterBot.py" && \
+python ./playgame.py -l game_logs -m maps/maze/maze_p02_01.map -e --strict \
+"./jockbot_ants_bot $@" "python sample_bots/python/HunterBot.py" && \
 cd ..
