f5e1dc1b4bae493682754173978bd0e67916fced
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2019-03-25 06:08:30 +0100

    Format integration tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,15 +1,14 @@
-extern crate jockbot_ants_bot;
 extern crate ants_ai_challenge_api;
+extern crate jockbot_ants_bot;
 
 use ants_ai_challenge_api::*;
 use jockbot_ants_bot::*;
 
 #[test]
 fn test_forced_to_walk_east() {
-
     let mut agent = FooAgent::default();
 
-    let params = GameParameters{
+    let params = GameParameters {
         cols: 6,
         rows: 3,
         attackradius2: 1,
@@ -18,20 +17,20 @@ fn test_forced_to_walk_east() {
         turns: 10,
         spawnradius2: 1,
         viewradius2: 3,
-        player_seed: 1244972669850148400,
+        player_seed: 1_244_972_669_850_148_400,
     };
 
     agent.prepare(params);
 
     // Ant sorounded by water except on east side.
     let world = WorldState::default()
-        .food(pos(1,5))
-        .live_ant(pos(1,1),0)
-        .water(pos(0,1))
-        .water(pos(1,0))
-        .water(pos(2,1));
+        .food(pos(1, 5))
+        .live_ant(pos(1, 1), 0)
+        .water(pos(0, 1))
+        .water(pos(1, 0))
+        .water(pos(2, 1));
 
-    let orders = agent.make_turn(world,1);
+    let orders = agent.make_turn(world, 1);
 
-    assert_eq![orders, vec![pos(1,1).order(Direction::East)]];
+    assert_eq![orders, vec![pos(1, 1).order(Direction::East)]];
 }
