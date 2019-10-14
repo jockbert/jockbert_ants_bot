b09fe3f00a082a7715952bf2350d4870be38e1bc
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2019-10-14 21:52:11 +0200

    Limit number of turns in test script to 150

--- test_my_bot.sh
+++ test_my_bot.sh
@@ -3,6 +3,6 @@
 RUST_BACKTRACE=1 cargo build && \
 cp target/debug/jockbot_ants_bot tools && \
 cd tools && \
-python ./playgame.py --turntime=5000 -l game_logs -m maps/maze/maze_p02_01.map -I -e -v --strict \
+python ./playgame.py --turntime=5000 --turns 150 -l game_logs -m maps/maze/maze_p02_01.map -I -e -v --strict \
 "./jockbot_ants_bot $@" "python sample_bots/python/HunterBot.py" && \
 cd ..
