da5074213891a8ec7894248e1058db6ecff5590d
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2020-04-05 22:50:22 +0200

    Change to narrower map in test_ny_bot.sh

--- test_my_bot.sh
+++ test_my_bot.sh
@@ -3,6 +3,6 @@
 RUST_BACKTRACE=1 cargo build && \
 cp target/debug/jockbot_ants_bot tools && \
 cd tools && \
-python ./playgame.py --turntime=5000 --turns 150 -l game_logs -m maps/maze/maze_p02_01.map -I -e -v --strict \
+python ./playgame.py --turntime=5000 --turns 80 -l game_logs -m maps/random_walk/random_walk_p02_32.map -I -e -v --strict \
 "./jockbot_ants_bot $@" "python sample_bots/python/HunterBot.py" && \
 cd ..
