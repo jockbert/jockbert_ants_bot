542a5b0a94e0e5888f498ea381bdf174827571ec
Joakim Åkerblom <per.joakim.eriksson@gmail.com>
2020-04-05 08:22:18 +0200

    Fix random walk bug
    Fix problem of thowing when there is no direction to choose in random
    walk.

--- src/strategy/random_walk.rs
+++ src/strategy/random_walk.rs
@@ -9,10 +9,15 @@ use std::collections::HashSet;
 pub struct RandomWalk {}
 
 /// Generates a random direction.
-fn random_direction(dirs: &[Direction]) -> Direction {
-    let mut rng = rand::thread_rng();
-    let index = rng.gen_range(0 as usize, dirs.len());
-    *dirs.get(index).expect("no out of bounds")
+fn random_direction(dirs: &[Direction]) -> Option<Direction> {
+    if dirs.is_empty() {
+        None
+    } else {
+        let mut rng = rand::thread_rng();
+        let index = rng.gen_range(0 as usize, dirs.len());
+        let dir = *dirs.get(index).expect("no out of bounds");
+        Some(dir)
+    }
 }
 
 impl Strategy for RandomWalk {
@@ -22,11 +27,35 @@ impl Strategy for RandomWalk {
         ants: &mut HashSet<Position>,
     ) -> Orders {
         ants.iter()
-            .map(|ant| {
-                ant.order(random_direction(
-                    world_step.available_directions(ant).as_ref(),
-                ))
+            .flat_map(|ant| {
+                random_direction(
+                    &world_step.available_directions(ant),
+                )
+                .map(|dir| ant.order(dir))
             })
             .collect()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::world_step::avoid_water_filter::*;
+    use std::iter::FromIterator;
+
+    #[test]
+    fn no_random_order_to_make() {
+        let world_step = AvoidWaterFilter::new_from_line_map(
+            "%%%
+             %a%
+             %%%",
+        );
+
+        let strategy = RandomWalk {};
+        let mut ants = HashSet::from_iter(
+            world_step.all_my_ants().iter().cloned(),
+        );
+
+        strategy.apply(&world_step, &mut ants);
     }
 }
